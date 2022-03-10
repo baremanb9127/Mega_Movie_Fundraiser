e89c9a72327597b5b83e6df9d3d04e50f2ee7ae4
baremanb9127 <100099633+baremanb9127@users.noreply.github.com>
2022-03-11 12:33:31 +1300

    Add files via upload

---
+++ 02_ticket_loop.py
@@ -0,0 +1,12 @@
+# Start of loop
+
+# Initialise loop so that it runs at least once
+Name = ""
+Count = 0
+MAX_TICKETS = 5
+
+while Name != "xxx" and Count < MAX_TICKETS:
+
+    # Get Details
+    name = input ("Name:  ")
+    Count += 1
